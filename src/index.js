b6025404721cf87c8d542c9c199a53f98d8c8765
Julia-M03 <yulya.mangura@mail.ru>
2025-08-31 19:51:42 +0300

    Node.js.Backend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-const http = require('http');
+const http = require("http");
+const path = require("path");
+const getUsers = require("./modules/users");
 
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, "http://127.0.0.1");
+  let otherParams = [];
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  for (const [key, value] of url.searchParams.entries()) {
+    if (key !== "hello") {
+      otherParams.push({ key, value });
+    }
+  }
 
+  if (url.searchParams.has("users")) {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.setHeader("Content-Type", "application/json");
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+
+  if (url.searchParams.has("hello")) {
+    const name = url.searchParams.get("hello");
+
+    if (!name) {
+      response.statusCode = 400;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Enter a name");
+      return;
+    }
+
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.end(`Hello, ${name}!`);
+    return;
+  }
+
+  if (otherParams.length > 0) {
+    response.statusCode = 500;
+    response.statusMessage = "Internal Server Error";
+    response.setHeader("Content-Type", "text/plain");
+    response.write("");
+    response.end();
+    return;
+  }
+
+  response.statusCode = 200;
+  response.statusMessage = "OK";
+  response.setHeader("Content-Type", "text/plain");
+  response.write("Hello, World!");
+  response.end();
+});
+
+server.listen(3003, () => {
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 });
